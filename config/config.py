0560293400b43d40c702f81adb551717c1f465cb
npragin <npragin@gmail.com>
2025-04-28 15:54:24 -0700

    Add hostnames to config file.

--- config/config.py
+++ config/config.py
@@ -1,2 +1,8 @@
+# Ports for communication between services
 ZMQ_PORT_FRONTEND_INGESTOR = 98700
 ZMQ_PORT_MODEL_INGESTOR = 98701
+
+# IPs for communication between services
+# NOTE: Only use localhost if the service is on the same machine as the data ingestor
+ZMQ_HOSTNAME_FRONTEND = "localhost"
+ZMQ_HOSTNAME_MODEL = "cn-gpu3.hpc.engr.oregonstate.edu"
